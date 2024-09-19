a463e2ba3c42d9594d6dc61f124bc0ca3ff58e2f
Carlos Alexandre Torrecilhas da Silva Sá <carlos.sa@ameh.org.br>
2024-09-19 13:14:13 -0300

    The PIS class was completed. The method print was tested and is working according with the desired

--- Atividade4/src/atividade4/Atividade4.java
+++ Atividade4/src/atividade4/Atividade4.java
@@ -8,6 +8,20 @@ public class Atividade4 {
     
     public static void main(String[] args) {
         ArrayList<Pis> listPis = new ArrayList<Pis>();
+        Scanner teclado = new Scanner(System.in);
+        double debito = 0;
+        double credito = 0;
+        
+        System.out.print("Informe o valor do debito: ");
+        debito = teclado.nextDouble();
+        System.out.print("Informe o valor do credito: ");
+        credito = teclado.nextDouble();
+        
+        Pis objPis = new Pis(debito, credito);
+        listPis.add(objPis);
+        listPis.get(0).imprimirImposto();
+        
+        
     }
     
 }
